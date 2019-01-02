d864483b989e3fd0942735f1fda29dd6dc39cc1a
jing.dong <dj3386083@gmail.com>
2019-01-02 21:23:42 +1100

    2nd update of 11

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -5,7 +5,29 @@ const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 
 const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelectorAll('[data-ship]');
+const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
 // Build out functions
+function togglePlay() {
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
+}
+
+function updateButton() {
+  //console.log('Update the button');
+  const icon = this.paused ? '►' : '❚ ❚'
+  console.log(icon);
+  toggle.textContent = icon;
+}
+
+function skip() {
+  console.log('skipping!');
+}
+
+// Hook up the event listners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', togglePlay);
+video.addEventListener('pause', togglePlay);
+
+toggle.addEventListener('click',togglePlay);
