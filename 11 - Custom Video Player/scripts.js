f7c4b980ff00eda036e39c2d2da47da1c6a48fe4
jing.dong <dj3386083@gmail.com>
2019-01-02 21:43:11 +1100

    FINAL

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,29 +1,26 @@
-// Get our elements
+/* Get Our Elements */
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
-
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
-// Build out functions
+/* Build out functions */
 function togglePlay() {
   const method = video.paused ? 'play' : 'pause';
   video[method]();
 }
 
 function updateButton() {
-  //console.log('Update the button');
-  const icon = this.paused ? '►' : '❚ ❚'
+  const icon = this.paused ? '►' : '❚ ❚';
   console.log(icon);
   toggle.textContent = icon;
 }
 
 function skip() {
-  console.log(this.dataset.skip);
-  video.currentTime += parseFloat (this.dataset.skip);
+ video.currentTime += parseFloat(this.dataset.skip);
 }
 
 function handleRangeUpdate() {
@@ -32,7 +29,7 @@ function handleRangeUpdate() {
 
 function handleProgress() {
   const percent = (video.currentTime / video.duration) * 100;
-  progressBar.style.flexBasic = `${percent}%`;
+  progressBar.style.flexBasis = `${percent}%`;
 }
 
 function scrub(e) {
@@ -40,13 +37,13 @@ function scrub(e) {
   video.currentTime = scrubTime;
 }
 
-// Hook up the event listners
+/* Hook up the event listeners */
 video.addEventListener('click', togglePlay);
-video.addEventListener('play', togglePlay);
-video.addEventListener('pause', togglePlay);
-video.addEventListener('timeupdate', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 
-toggle.addEventListener('click',togglePlay);
+toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
